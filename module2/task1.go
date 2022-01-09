4fc8d886af68a4a91acbff47f62d31f7f3b071ad
henry <island_1123@163.com>
2022-01-10 00:23:43 +0800

    fix-word

--- module2/task1.go
+++ module2/task1.go
@@ -15,7 +15,7 @@ func Server() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		str, err := json.Marshal(r.Header)
 		if err != nil {
-			panic(errors.New("header jsonize eror"))
+			panic(errors.New("header jsonize error"))
 		}
 		w.Header().Set("Location", r.URL.Path)
 		w.WriteHeader(http.StatusOK)
@@ -27,7 +27,7 @@ func Server() {
 		fmt.Println(r.Host)
 		w.Write([]byte("Os: " + runtime.GOOS + runtime.GOARCH + "\n"))
 		w.Write([]byte("Go Version: " + runtime.Version() + "\n"))
-		w.Write([]byte("Requet Path: " + r.URL.Path + "\n"))
+		w.Write([]byte("Request Path: " + r.URL.Path + "\n"))
 		w.Write([]byte("Http Code: " + strconv.Itoa(http.StatusOK)))
 	})
 
