ef0eadbce2191010f1cc6cee73c1313a432e3cf9
henry <island_1123@163.com>
2022-01-10 00:22:27 +0800

    add-test

--- module2/task1.go
+++ module2/task1.go
@@ -3,20 +3,33 @@ package module2
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
+	"runtime"
+	"strconv"
 )
 
 func Server() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		str, err := json.Marshal(r.Header)
-		if err == nil {
+		if err != nil {
 			panic(errors.New("header jsonize eror"))
 		}
 		w.Header().Set("Location", r.URL.Path)
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte(str))
 	})
+
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Host)
+		w.Write([]byte("Os: " + runtime.GOOS + runtime.GOARCH + "\n"))
+		w.Write([]byte("Go Version: " + runtime.Version() + "\n"))
+		w.Write([]byte("Requet Path: " + r.URL.Path + "\n"))
+		w.Write([]byte("Http Code: " + strconv.Itoa(http.StatusOK)))
+	})
+
 	log.Fatal(http.ListenAndServe("localhost:9000", nil))
 }
